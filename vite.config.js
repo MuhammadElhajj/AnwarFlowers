7660dfdc788ef24a2d0290f59bf43b18ea849a5e
MuhammadElhajj <3muhammad.el.hajj@gmail.com>
2026-05-08 13:48:23 +0300

    Change

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+
+// export default defineConfig({
+//   plugins: [react()],
+  
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  //  base: '/AnwarFlowers/',
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'Safari >= 13'],
+      modernPolyfills: true,
+    }),
+  ],
 })
